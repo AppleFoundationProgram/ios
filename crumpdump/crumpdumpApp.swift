d75fcf2e62e1da757db76f255fe4e9f0f1614776
delightpip <devdelightpip@gmail.com>
2024-10-14 14:54:15 +0900

    Initial Commit

---
+++ crumpdump/crumpdumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  crumpdumpApp.swift
+//  crumpdump
+//
+//  Created by Jiwon Kim on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct crumpdumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
